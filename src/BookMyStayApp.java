b62e0b407f6520a454ba68c6c1e07a7992277279
Layatmika-27 <blayatmika08@gmail.com>
2026-03-16 10:58:16 +0530

    2nd usecase created

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,5 +1,92 @@
+abstract class Room {
+
+    private String roomType;
+    private int numberOfBeds;
+    private int size;
+    private double pricePerNight;
+
+    public Room(String roomType, int numberOfBeds, int size, double pricePerNight) {
+        this.roomType = roomType;
+        this.numberOfBeds = numberOfBeds;
+        this.size = size;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + size + " sq ft");
+        System.out.println("Price per Night: $" + pricePerNight);
+    }
+}
+
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 200, 100);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 350, 180);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 500, 350);
+    }
+}
+
 public class BookMyStayApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to BookMyStay App!");
+
+        // Create Room Objects (Polymorphism)
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        // Availability Variables (No Data Structures)
+        int singleRoomAvailability = 5;
+        int doubleRoomAvailability = 3;
+        int suiteRoomAvailability = 2;
+
+        System.out.println("Welcome to BookMyStay\n");
+
+        System.out.println("---- Room Details ----");
+
+        singleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleRoomAvailability);
+        System.out.println();
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleRoomAvailability);
+        System.out.println();
+
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteRoomAvailability);
+        System.out.println();
+
+        System.out.println("Application Terminated.");
     }
 }
